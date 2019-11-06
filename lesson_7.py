e369e2b5a856bf25f25cc12f09674e57612fdd2d
Artem Pixaykin <artem.pixaykin@yandex.ru>
2019-11-06 23:07:36 +0500

    another commit

--- lesson_7.py
+++ lesson_7.py
@@ -1,9 +1,16 @@
+'''
 a = [[int(i) for i in input().split()]]     #считываем первую строку
 b = input()                                 #читаем вторую строку
 while b != 'end' :                          #заходим в цикл и читаем данные пока не введут end
     a.append([int(i) for i in b.split()])   #добавляем введённое значение в конец списка a
     b = input()                             #считываем новое значение
+'''
 
-for i in a:
-    for j in i:
-        print(j)
+a = [[9, 5, 3], [0, 7, -1], [-5, 2, 9]]
+c = []
+for i in range(len(a)):
+    for j in range(len(a[i])):
+        #c[i][j] += a[i-1][j] + a[(i+1)%len(a)][j] + a[i][j-1] + a[i][(j+1)%len(a)]
+        print(a[i-1][j] + a[(i+1)%len(a)][j] + a[i][j-1] + a[i][(j+1)%len(a[i])], end=' ')
+    print()    
+
